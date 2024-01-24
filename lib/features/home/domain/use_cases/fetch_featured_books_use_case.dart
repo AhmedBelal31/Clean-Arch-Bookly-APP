89e0dfbeeff7a1df966caba618422939d4196eb2
Ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-01-24 12:45:18 +0200

    fetch Newest Books use case

--- lib/features/home/domain/use_cases/fetch_featured_books_use_case.dart
+++ lib/features/home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -4,24 +4,19 @@ import 'package:clean_arch_bookly_app/features/home/domain/repos/home_repo.dart'
 import 'package:dartz/dartz.dart';
 import '../../../../core/use_case/use_case.dart';
 
+class FetchFeaturedBooksUseCase extends UseCase<List<BookEntity>, void> {
+  HomeRepo homeRepo;
 
-
-
-class FetchFeaturedBooksUseCase extends UseCase<List<BookEntity> , void>
-{
-  HomeRepo homeRepo ;
   FetchFeaturedBooksUseCase({required this.homeRepo});
 
   @override
-  Future<Either<Failure, List<BookEntity>>> execute([void param])  async{
-    return await homeRepo.fetchFeaturedBooks() ;
+  Future<Either<Failure, List<BookEntity>>> execute([void param]) async {
+    return await homeRepo.fetchFeaturedBooks();
   }
 
-  // Future<Either<Failure , List<BookEntity>>> execute() async
-  // {
-  //   // Maybe I can do something in the future , 'Check Permissions'
-  //   return await homeRepo.fetchFeaturedBooks() ;
-  // }
-
-
+// Future<Either<Failure , List<BookEntity>>> execute() async
+// {
+//   // Maybe I can do something in the future , 'Check Permissions'
+//   return await homeRepo.fetchFeaturedBooks() ;
+// }
 }
