083b92083770201a174518768567a335320e2644
Ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-01-24 11:00:44 +0200

    implement book entity

---
+++ lib/features/home/domain/entities/book_entity.dart
@@ -0,0 +1,19 @@
+class BookEntity {
+  String imageUrl;
+
+  String title;
+
+  String authorName;
+
+  String price;
+
+  String rating;
+
+  BookEntity({
+    required this.imageUrl,
+    required this.title,
+    required this.authorName,
+    required this.price,
+    required this.rating,
+  });
+}
