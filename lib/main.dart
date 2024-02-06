b8dcf314ec5b20197f6f3da97edb9979747ed44a
Ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-02-06 20:03:26 +0200

    Register book entity adapter and open featured box

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:clean_arch_bookly_app/features/home/domain/entities/book_entity.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'const.dart';
 import 'core/utils/app_router.dart';
 import 'core/utils/bloc_observer.dart';
@@ -9,9 +11,12 @@ import 'core/utils/service_locator.dart';
 // import 'features/home/presentation/view_models/featured_books_cubit/featured_books_cubit.dart';
 // import 'features/home/presentation/view_models/newest_books_cubit/newest_books_cubit.dart';
 
-void main() {
+void main()async {
   // ApiService.init();
   // setupServiceLocator();
+ await Hive.initFlutter();
+  Hive.registerAdapter(BookEntityAdapter());
+  await Hive.openBox(kFeaturedBox);
   Bloc.observer = MyBlocObserver();
   runApp(const MyApp());
 }
